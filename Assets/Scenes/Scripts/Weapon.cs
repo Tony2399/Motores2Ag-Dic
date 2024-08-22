a235882c5222a806322f5fd8c322cab297912d5c
Tony2399 <163464729+Tony2399@users.noreply.github.com>
2024-08-21 21:52:15 -0600

    Balas

---
+++ Assets/Scenes/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bulletPrefab;
+    public Transform spawnPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            GameObject NewBullet = Instantiate(bulletPrefab);
+            NewBullet.transform.position = spawnPoint.position;
+            NewBullet.transform.rotation = spawnPoint.rotation;
+            NewBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.transform.forward*200.0f);
+            
+        }
+        
+    }
+}
