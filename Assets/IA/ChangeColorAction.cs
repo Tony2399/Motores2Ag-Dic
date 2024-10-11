f012e747230219757a55229f09dd95afb595f3a8
oakpoak <163464729+Tony2399@users.noreply.github.com>
2024-10-10 21:50:55 -0600

    d342

---
+++ Assets/IA/ChangeColorAction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorAction : AIAction
+{
+    public override void PerformAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
